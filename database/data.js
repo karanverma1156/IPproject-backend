5b4994a6dccfb9159ccd3b27e44d36813f849539
karanverma1156 <80563519+karanverma1156@users.noreply.github.com>
2023-05-10 20:20:28 +0530

    Add files via upload

---
+++ database/data.js
@@ -0,0 +1,149 @@
+export default [
+    {
+        id: 1,
+        question : "Javascript is an _______ language",
+        options : [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 2,
+        question : "Following methods can be used to display data in some form using Javascript",
+        options : [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 3,
+        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options : [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 4,
+        question : "What does the toString() method return?",
+        options : [
+            'Return Object',
+            'Return String',
+            'Return Integer',
+        ]
+    },
+    {
+        id: 5,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    
+    {
+        id: 6,
+        question : "Which of the following keywords is used to define a variable in Javascript?",
+        options : [
+            'var',
+            'let',
+            'Both A & B',
+        ]
+    },
+
+    {
+        id: 7,
+        question : "Which of the following methods is used to access HTML elements using Javascript?",
+        options : [
+            'getElementbyId()',
+            'Both A & B',
+            'getElementsByClassName()',
+        ]
+    },
+
+    {
+        id: 8,
+        question : "How can a datatype be declared to be a constant type?",
+        options : [
+            'const',
+            'var',
+            'constant',
+        ]  
+    },
+
+    {
+        id: 9,
+        question : "What keyword is used to check whether a given property is valid or not?",
+        options : [
+            'exists',
+            'is in',
+            'in',
+        ]   
+    },
+
+    {
+        id: 10,
+        question : "Which function is used to serialize an object into a JSON string in Javascript?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]   
+    },
+
+    {
+        id: 11,
+        question : "Which of the following are closures in Javascript?",
+        options : [
+            'Functions',
+            'All of the above',
+            'Variables',
+        ]   
+    },
+
+    {
+        id: 12,
+        question : "Which of the following is not a Javascript framework?",
+        options : [
+            'Cassandra',
+            'Vue',
+            'React',
+        ]   
+    },
+
+    {
+        id: 13,
+        question : "What keyword is used to declare an asynchronous function in Javascript?",
+        options : [
+            'None of the above',
+            'await',
+            'async',
+        ]   
+    },
+
+    {
+        id: 14,
+        question : "How to stop an interval timer in Javascript?",
+        options : [
+            'clearTimer',
+            'clearInterval',
+            'intervalOver',
+        ]    
+    },
+
+    {
+        id: 15,
+        question : "What does … operator do in JS?",
+        options : [
+            'No such operator exists',
+            'It is used to spread iterables to individual elements',
+            'It is used to describe a datatype of undefined size',
+        ]    
+    }
+];
+
+export const answers = [0, 1, 2, 1, 0, 2, 1, 0, 2, 0, 1, 0, 2, 1, 1];
